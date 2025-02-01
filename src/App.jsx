bf67304c9f66f47130c75722e36301c2c9e99c68
marco-oliveira7 <ma8155354@gmail.com>
2025-01-31 22:45:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 
 function App() {
-
-  const [img, setImg] = useState('')
-  const [download, setDownload] = useState('')
+  const [img, setImg] = useState("");
+  const [download, setDownload] = useState(
+    "https://api.qrserver.com/v1/create-qr-code/?size=288x288&data=ok"
+  );
 
   function generateQR() {
-    const word = document.querySelector("#word").value;
+    const word = document.querySelector("#word");
+    if (word.value === "") {
+      word.style.border = "1px solid red";
+    } else {
+      word.style.border = "";
+      const code = document.querySelector(".code");
+      code.style.height = "376px";
+      code.style.opacity = "1";
 
-    const code = document.querySelector(".code");
-    code.style.height = "376px";
-    code.style.opacity = "1";
-
-    setImg(`https://api.qrserver.com/v1/create-qr-code/?size=288x288&data=${word}`)
+      setImg(
+        `https://api.qrserver.com/v1/create-qr-code/?size=288x288&data=${word.value}`
+      );
+    }
   }
 
   return (
     <div className="flex justify-center items-center bg-gray-900 h-screen w-screen">
-      <iframe name="iframe_download" className="hidden"></iframe>
       <div className="bg-white px-10 py-8 rounded-3xl">
         <input
           type="text"
@@ -32,13 +38,11 @@ function App() {
           Generate QR
         </button>
         <div className="flex flex-col items-center h-0 opacity-0 code">
-          <img
-            src={img}
-            alt=""
-            className="w-72 h-72 my-7"
-          />
-          <a href={img} download="QRCode.png" >
-            clique
+          <img src={img} alt="" className="w-72 h-72 my-7" />
+          <a href={img} download="QRCode">
+            <button className="bg-green-600 text-white rounded-lg px-2 py-1">
+              Download
+            </button>
           </a>
         </div>
       </div>
