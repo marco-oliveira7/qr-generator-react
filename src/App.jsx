e766a930ede3b30db6756eded76ed7bcda81247a
marco-oliveira7 <ma8155354@gmail.com>
2025-01-31 22:17:22 -0300

    Function GenerateQR implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
 function App() {
+
+  const [img, setImg] = useState('')
+  const [download, setDownload] = useState('')
+
   function generateQR() {
-    const code = document.querySelector(".code")
-    code.style.height = '376px'
-    code.style.opacity = '1'
+    const word = document.querySelector("#word").value;
+
+    const code = document.querySelector(".code");
+    code.style.height = "376px";
+    code.style.opacity = "1";
+
+    setImg(`https://api.qrserver.com/v1/create-qr-code/?size=288x288&data=${word}`)
   }
 
   return (
     <div className="flex justify-center items-center bg-gray-900 h-screen w-screen">
+      <iframe name="iframe_download" className="hidden"></iframe>
       <div className="bg-white px-10 py-8 rounded-3xl">
         <input
           type="text"
+          id="word"
           placeholder="type the URL here"
           className="bg-gray-300 text-black mr-4 px-2 py-1 rounded-md"
         />
@@ -23,13 +33,13 @@ function App() {
         </button>
         <div className="flex flex-col items-center h-0 opacity-0 code">
           <img
-            src="https://afinz.com.br/wp-content/uploads/2022/04/QR_Code_Afinz.png"
+            src={img}
             alt=""
             className="w-72 h-72 my-7"
           />
-          <button className="bg-green-700 text-white rounded-lg px-2 py-1 ">
-            Download
-          </button>
+          <a href={img} download="QRCode.png" >
+            clique
+          </a>
         </div>
       </div>
     </div>
